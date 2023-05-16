3ec7cf24068f63432c4981b47e03ccfac66a8eb9
richie <113203265+R1chi333@users.noreply.github.com>
2023-05-15 22:09:35 -0400

    .

---
+++ souprun/src/components/container.vue
@@ -0,0 +1,25 @@
+<template>
+    <canvas ref="canvas"></canvas>
+</template>
+  
+<script>
+
+  export default {
+    mounted() {
+      const canvas = this.$refs.canvas;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight
+      const ctx = canvas.getContext('2d');
+  
+      // Create our image
+      const newImage = new Image();
+      newImage.src = '/bowl.png';
+  
+      // When it loads
+      newImage.onload = () => {
+        // Draw the image onto the context
+        ctx.drawImage(newImage, 500, 0);
+      };
+    },
+  };
+</script>
--- souprun/src/views/HomeView.vue
+++ souprun/src/views/HomeView.vue
@@ -1,17 +1,30 @@
 <script setup>
 import leaderboard from '../components/leaderboard.vue'
+import container from '../components/container.vue'
 </script>
 
 <template>
   <main>
     <div>
       <h1 id="title">SOUP RUN</h1>
-      <leaderboard />
+      <div id="leaderboard">
+        <leaderboard />
+      </div>
+      <div id="con">
+        <container/>
+      </div>
     </div>
   </main>
 </template>
 <style scoped>
+#leaderboard{
+  position: absolute;
+  left: 2vw;
+}
 #title {
   text-align: center;
+}
+#con{
+  position: absolute;
 }
 </style>
