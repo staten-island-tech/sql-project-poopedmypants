ed766968e3420bd5c4b8ff8f3b9915767d29d39d
jessychui <jessy.chui@gmail.com>
2023-05-22 13:58:59 -0400

    supabussy installed
    HUJK

---
+++ souprun/src/components/loginPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <input type="text" />
+    <input type="text" />
+    <button class="sign-up"></button>
+    <button class="log-in"></button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
