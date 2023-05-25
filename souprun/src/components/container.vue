dbb3992b1ee904d1856be9a80f482d668bb5653c
richie <113203265+R1chi333@users.noreply.github.com>
2023-05-25 04:00:45 -0400

    .

--- souprun/src/components/container.vue
+++ souprun/src/components/container.vue
@@ -1,110 +1,161 @@
 <template>
-  <canvas ref="canvas" @click="clickedthebunny"></canvas>
+  <div>
+    <canvas id="con"></canvas>
+  </div>
 </template>
 
 <script>
-let x = 500
-let y = 20
-let x2 = 1250
-let choice = 0
-let characters = ['/cat.png', '/bun.png', '/hippo.png']
-let chara = characters[choice]
-
 export default {
-  props: {
-    started: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    resize() {
-      const canvas = this.$refs.canvas
-      canvas.width = window.innerWidth
-      canvas.height = window.innerHeight
-      this.ctx = canvas.getContext('2d')
-
-      this.newImage = new Image()
-      this.newImage.src = '/bowl.png'
-      this.bun = new Image()
-      this.bun.src = chara
-
-      this.bun.onload = () => {
-        this.animate()
-      }
-      this.newImage.onload = () => {
-        this.animate()
-      }
-    },
-    startGame() {
-      console.log('start')
-    },
-    animate() {
-      let extra = window.innerWidth + window.innerWidth
-      this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-      this.ctx.drawImage(this.newImage, x, 0)
-      this.ctx.drawImage(this.bun, x2, y, 200, 150)
-
-      if (y < 301) {
-        y += 5
-      } else {
-        x -= 7
-        if (x2 >= 250) {
-          x2 -= 7
-        }
-      }
-
-      if (x >= -1900 && this.started) {
-        requestAnimationFrame(this.animate)
-      }
-    },
-    clickedthebunny(event) {
-      const clickX = event.clientX
-      const clickY = event.clientY
-
-      if (
-        clickX >= x2 &&
-        clickX <= x2 + 290 &&
-        clickY >= y &&
-        clickY <= y + 200 &&
-        this.started === false
-      ) {
-        choice += 1
-        this.bun.src = characters[choice % characters.length]
-        y = 20
-      }
-    }
-  },
-  watch: {
-    started: {
-      immediate: true,
-      handler(newValue, oldValue) {
-        console.log('Prop value changed:', newValue)
-
-        if (newValue) {
-          this.animate()
-        }
-      }
-    }
+  data() { //very sorry for all these variables .-.
+    return {
+      canvas: null,
+      ctx: null,
+      conRect: null,
+      conBottom: null,
+      bowlX: null,
+      bowlHeight: null,
+      bowlWidth: null,
+      bowl: null,
+      newBowlX: null,
+      started: false,
+      startBtnX: null,
+      startBtnHeight: null,
+      startBtnWidth: null,
+      startBtn: null,
+      startBtnY: null,
+      avatarList: null,
+      avatar: null,
+      avatarIndex: 0,
+      newAvatarY: null,
+    };
   },
   mounted() {
-    const canvas = this.$refs.canvas
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-    this.ctx = canvas.getContext('2d')
-
-    this.newImage = new Image()
-    this.newImage.src = '/bowl.png'
-    this.bun = new Image()
-    this.bun.src = chara
-
-    this.bun.onload = () => {
-      this.animate()
+    this.canvas = document.querySelector('canvas');
+    this.canvas.width = window.innerWidth * 0.75;
+    this.canvas.height = this.canvas.width * 0.6;
+    window.addEventListener('resize', this.resizeplz);
+    this.ctx = this.canvas.getContext('2d')
+    this.conRect = this.canvas.getBoundingClientRect();
+    this.conBottom = this.conRect.bottom
+    this.canvas.addEventListener('click', this.startButtonClick);
+    this.canvas.addEventListener('click', this.avatarClick);
+    this.makeBowl();
+    this.makeStartButton();
+    this.avatarList = ['/bun.png', '/cat.png', '/hippo.png']
+    this.makeAvatar();
+  },
+  methods: {
+    resizeplz() {
+      this.canvas.width = window.innerWidth * 0.75;
+      this.canvas.height = this.canvas.width * 0.6;
+      this.conBottom = this.canvas.height - this.bowlHeight;
+      this.bowlWidth = this.canvas.width + (this.canvas.width/2)
+      this.bowlHeight = this.canvas.height * .75;
+      this.bowlX = this.canvas.width/3
+      this.newBowlX = this.bowlX/-.7
+      this.startBtnWidth = this.canvas.width/4
+      this.startBtnHeight = this.canvas.width/9;
+      this.startBtnX = this.canvas.width * .09
+      this.startBtnY = this.conBottom + this.canvas.width * .23
+      if(this.started === true){
+        this.ctx.drawImage(this.bowl, this.newBowlX, this.conBottom, this.bowlWidth, this.bowlHeight);
+      }else{
+        this.ctx.drawImage(this.bowl, this.bowlX, this.conBottom, this.bowlWidth, this.bowlHeight);
+        this.ctx.drawImage(this.startBtn,this.startBtnX,this.startBtnY,this.startBtnWidth,this.startBtnHeight);
+      }
+    },
+    makeAvatar(){
+      this.avatar = new Image();
+      this.avatar.src = this.avatarList[this.avatarIndex]
+      this.avatar.onload = () => {
+        this.ctx.drawImage(this.avatar,this.avatarX,this.avatarY,this.avatarWidth,this.avatarHeight);
+      };
+      this.avatarWidth = this.canvas.width/5
+      this.avatarHeight = this.canvas.height/4;
+      this.avatarX = this.canvas.width/1.5
+      this.avatarY = this.conBottom - this.canvas.width * .1
+      this.newAvatarY = this.conBottom + this.canvas.width * .05
+    },
+    makeStartButton(){
+      this.startBtn = new Image();
+      this.startBtn.src = "/start.png"; //placeholderrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr ahhhh
+      this.startBtn.onload = () => {
+        this.ctx.drawImage(this.startBtn,this.startBtnX,this.startBtnY,this.startBtnWidth,this.startBtnHeight);
+      };
+      this.startBtnWidth = this.canvas.width/4
+      this.startBtnHeight = this.canvas.width/9;
+      this.startBtnX = this.canvas.width * .09
+      this.startBtnY = this.conBottom + this.canvas.width * .23
+    },
+    startButtonClick(event) {
+      const clickX = event.offsetX;
+      const clickY = event.offsetY;
+    if (
+      clickX >= this.startBtnX &&
+      clickX <= this.startBtnX + this.startBtnWidth &&
+      clickY >= this.startBtnY &&
+      clickY <= this.startBtnY + this.startBtnHeight
+    ) {
+      this.moveBowl(); // Execute moveBowl function
     }
-    this.newImage.onload = () => {
-      this.animate()
+  },
+    avatarClick(event){
+      const clickX = event.offsetX;
+      const clickY = event.offsetY;
+      if (
+      clickX >= this.avatarX &&
+      clickX <= this.avatarX + this.avatarWidth &&
+      clickY >= this.avatarY &&
+      clickY <= this.avatarY + this.avatarHeight
+    ) {
+      this.avatarIndex += 1;
+      this.avatar.src = this.avatarList[this.avatarIndex % this.avatarList.length]
+      this.ctx.clearRect(this.avatarX, this.avatarY, this.avatarWidth, this.avatarHeight);
     }
-    window.onresize = this.resize()
+    },
+    makeBowl() {
+      this.bowl = new Image();
+      this.bowl.src = '/bowl.png';
+
+      this.bowl.onload = () => {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.drawImage(this.bowl, this.bowlX, this.conBottom, this.bowlWidth, this.bowlHeight);
+      };
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.bowlWidth = this.canvas.width + (this.canvas.width/2)
+      this.bowlHeight = this.canvas.height * .75;
+      this.bowlX = this.canvas.width/3
+      this.conBottom = this.canvas.height - this.bowlHeight;
+      this.newBowlX = this.bowlX/-.7
+    },
+    moveBowl(){
+      if (this.bowlX > this.newBowlX){
+        requestAnimationFrame(this.moveBowl);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(this.bowl, this.bowlX, this.conBottom, this.bowlWidth, this.bowlHeight);
+      let moveBowlSpeed = this.canvas.width/200
+      setTimeout(() => {
+        this.bowlX -= moveBowlSpeed
+      }, 1000);
+      this.startBtnY -= moveBowlSpeed
+      if(this.avatarY <= this.conBottom + this.canvas.width * .05){
+        this.avatarY += moveBowlSpeed/2
+      }
+      this.started = true
+      this.ctx.drawImage(this.startBtn,this.startBtnX,this.startBtnY,this.startBtnWidth,this.startBtnHeight);
+      this.ctx.drawImage(this.avatar,this.avatarX,this.avatarY,this.avatarWidth,this.avatarHeight);
+      }
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+#con {
+  background-color: rgb(255, 248, 226);
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  top: 50%;
+}
+</style>
--- souprun/src/views/HomeView.vue
+++ souprun/src/views/HomeView.vue
@@ -1,25 +1,16 @@
 <script setup>
-import leaderboard from '../components/leaderboard.vue'
 import container from '../components/container.vue'
-import startBtn from '../components/startBtn.vue'
 import login from '../components/login.vue'
 </script>
 
 <template>
   <main>
     <div>
-      <div id="titleCon" :class="{ 'slide-up': started }">
-        <h1 id="title">SOUP RUN</h1>
         <login id="login"/>
-      </div>
-      <div id="leaderboard" :class="{ 'slide-out': started }">
-        <leaderboard :started="started"/>
-        <startBtn id="start" :started="started" @update:started="started = $event" />
       </div>
       <div id="con">
         <container :started="started"/>
       </div>
-    </div>
   </main>
 </template>
 
@@ -39,40 +30,5 @@ export default {
   top: 2%;
   right: 2%;
   z-index: 55;
-}
-.slide-out {
-  transform: translateX(-150%);
-}
-.slide-up {
-  transform: translateY(-100vh);
-}
-#start{
-  position: absolute;
-  right: 50%;
-  z-index: 99;
-  margin-top: 3vw;
-}
-#leaderboard{
-  position: absolute;
-  left: 4vw;
-  z-index: 2;
-  transition: transform 1s ease-in-out;
-  top: 25%;
-}
-#title {
-  left: 2%;
-  font-family: 'Bubblegum Sans', cursive;
-  position: absolute;
-  font-size: 8vw;
-  margin: 0;
-  margin-top: 2.5vw;
-  color: #681616;
-}
-#titleCon{
-  transition: transform 1s ease-in-out;
-}
-#con{
-  position: absolute;
-  z-index: 1;
 }
 </style>
